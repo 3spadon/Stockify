8ae323a02681c75cc48b13a2ffa5aacd7a6f5899
3spadon <zevoboss@gmail.com>
2020-06-26 04:04:49 +0200

    V0.7.2

--- authentication.php
+++ authentication.php
@@ -24,7 +24,7 @@ if (!empty($_POST['username']) && !empty($_POST['password'])){ //Si les champs p
     if (!$result = $mysqli->query($sql)) {
     echo "<h2>La requête SQL est malformée..</h2>";
     echo"A des fins de sécurité, votre adresse IP et les informations de votre navigateur ont été sauvegardées et envoyées à notre administrateur.<br>";
-    echo"Si cette requête avais une fin malveillante, <mark>nous vous invitons vivement</mark> à cesser ces tentatives d'injections !<br><br><br>";
+    echo"Si cette requête avait une fin malveillante, <mark>nous vous invitons vivement</mark> à cesser ces tentatives d'injections !<br><br><br>";
     echo"<h4>Pour rappel</h4>";
     echo"Le délit d'accès frauduleux dans un système de traitement automatisé de données est prévu et réprimé par l'article 323-1 du Code pénal aux termes duquel <br><b>&lsaquo;&lsaquo; le fait d'accéder ou de se maintenir, frauduleusement, dans tout ou partie d'un système de traitement automatisé de données est puni de deux ans d'emprisonnement et de 30 000 euros d'amende &rsaquo;&rsaquo; </b>.<br> A savoir que la protection d‚Äôun syst√®me de traitement automatis√© de donn√©es par un dispositif de sécurité n'est pas une condition de l'incrimination.";
     }
